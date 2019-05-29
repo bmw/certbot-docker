b2c57325fc2c27a5a86743f0fc478b5a46144d62
Adrien Ferrand <ferrand.ad@gmail.com>
2019-05-29 13:55:10 +0200

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+# This script deploys a new version of certbot dockers (core+plugins) regarding a released version of Certbot.
+# The README.md is updated to include the reference of this new version, and a tag version is pushed to the
+# Certbot Docker repository, triggering the DockerHub autobuild feature that will take care of the release.
+
+# Usage: ./deploy.sh [VERSION]
+#   with [VERSION] corresponding to a released version of certbot, like `v0.34.0`
+
 trap Cleanup 1 2 3 6
 
 Cleanup() {
@@ -17,8 +24,8 @@ sed -i -e "s|current-.*-blue\.svg|current-$CERTBOT_VERSION-blue.svg|g" README.md
 sed -i -e "s|branch=.*)\]|branch=$BRANCH_NAME)]|g" README.md
 
 pushd "$WORK_DIR"
-    git commit -a -m "Release version $CERTBOT_VERSION"
-    git tag "$BRANCH_NAME"
+    git commit -a -m "Release version $CERTBOT_VERSION" --allow-empty
+    git tag "$CERTBOT_VERSION"
     git push
-    git push "$BRANCH_NAME"
+    git push --tags
 popd
