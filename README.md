530b6ea1ed59f33341f340b9d0f441783d129981
Adrien Ferrand <ferrand.ad@gmail.com>
2019-05-29 12:06:17 +0200

    Add a nice README, and a complete build script

---
+++ README.md
@@ -0,0 +1,28 @@
+# ![](https://certbot.eff.org/images/certbot-logo-1A.svg)
+[![](https://img.shields.io/badge/current-0.34.0-blue.svg)](https://github.com/certbot/certbot.git) &nbsp; [![](https://travis-ci.com/certbot/certbot.svg?branch=0.34.x)](https://travis-ci.com/certbot/certbot)
+
+## Certbot core
+
+* [certbot](https://hub.docker.com/r/certbot/certbot)
+
+## Certbot DNS plugins
+
+* [dns-dnsmadeeasy](https://hub.docker.com/certbot/dns-dnsmadeeasy)
+* [dns-dnsimple](https://hub.docker.com/certbot/dns-dnsimple)
+* [dns-ovh](https://hub.docker.com/certbot/dns-ovh)
+* [dns-cloudflare](https://hub.docker.com/certbot/dns-cloudflare)
+* [dns-cloudxns](https://hub.docker.com/certbot/dns-cloudxns)
+* [dns-digitalocean](https://hub.docker.com/certbot/dns-digitalocean)
+* [dns-google](https://hub.docker.com/certbot/dns-google)
+* [dns-luadns](https://hub.docker.com/certbot/dns-luadns)
+* [dns-nsone](https://hub.docker.com/certbot/dns-nsone)
+* [dns-rfc2136](https://hub.docker.com/certbot/dns-rfc2136)
+* [dns-route53](https://hub.docker.com/certbot/dns-route53)
+* [dns-gehirn](https://hub.docker.com/certbot/dns-gehirn)
+* [dns-linode](https://hub.docker.com/certbot/dns-linode)
+* [dns-sakuracloud](https://hub.docker.com/certbot/dns-sakuracloud)
+
+Source:
+
+* [docker] (https://www.github.com/certbot/certbot-docker.git)
+* [certbot] (https://www.github.com/certbot/certbot.git)
---
+++ build.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# This script builds certbot docker and certbot dns plugins docker against a given release version of certbot.
+# The build is done following the environment used by Dockerhub to handle its autobuild feature, and so can be
+# used as a pre-deployment validation test.
+
+# Usage: ./build.sh [VERSION]
+#   with [VERSION] corresponding to a released version of certbot, like `v0.34.0`
+
+trap Cleanup 1 2 3 6
+
+Cleanup() {
+    if [ ! -z "$WORK_DIR" ]; then
+        rm -rf "$WORK_DIR/plugin/certbot" || true
+        rm -rf "$WORK_DIR/core/certbot" || true
+    fi
+}
+
+WORK_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+DOCKER_TAG="$1"
+SOURCE_BRANCH="$DOCKER_TAG"
+ 
+StandardHubBuild() {
+    local L_IMAGE_NAME="$1"
+    local L_CONTEXT_PATH="$2"
+    local L_DOCKERFILE_RELATIVE_PATH="$3"
+    local L_DOCKERFILE_PATH="$L_CONTEXT_PATH/$L_DOCKERFILE_RELATIVE_PATH"
+    docker build -f "$L_DOCKERFILE_PATH" -t "$L_IMAGE_NAME" "$L_CONTEXT_PATH"
+}
+
+Cleanup
+
+# Step 1: Certbot core Docker
+
+DOCKER_REPO="certbot/certbot"
+CONTEXT_PATH="$WORK_DIR/core"
+DOCKERFILE_RELATIVE_PATH="Dockerfile"
+IMAGE_NAME="$DOCKER_REPO:$DOCKER_TAG"
+
+pushd "$CONTEXT_PATH"
+    SOURCE_BRANCH="$SOURCE_BRANCH" bash hooks/pre_build
+    StandardHubBuild "$IMAGE_NAME" "$(pwd)" "$DOCKERFILE_RELATIVE_PATH"
+popd
+
+Cleanup
+
+# Step 2: Certbot dns plugins Dockers
+
+CERTBOT_PLUGINS_DOCKER_REPOS=(
+    "certbot/dns-dnsmadeeasy"
+    "certbot/dns-dnsimple"
+    "certbot/dns-ovh"
+    "certbot/dns-cloudflare"
+    "certbot/dns-cloudxns"
+    "certbot/dns-digitalocean"
+    "certbot/dns-google"
+    "certbot/dns-luadns"
+    "certbot/dns-nsone"
+    "certbot/dns-rfc2136"
+    "certbot/dns-route53"
+    "certbot/dns-gehirn"
+    "certbot/dns-linode"
+    "certbot/dns-sakuracloud"
+)
+
+for DOCKER_REPO in ${CERTBOT_PLUGINS_DOCKER_REPOS[@]}; do
+    CONTEXT_PATH="$WORK_DIR/plugin"
+    DOCKERFILE_RELATIVE_PATH="Dockerfile"
+    IMAGE_NAME="$DOCKER_REPO:$DOCKER_TAG"
+
+    pushd "$CONTEXT_PATH"
+        SOURCE_BRANCH="$SOURCE_BRANCH" DOCKER_TAG="$DOCKER_TAG" bash hooks/pre_build
+        SOURCE_BRANCH="$SOURCE_BRANCH" DOCKER_TAG="$DOCKER_TAG" DOCKER_REPO="$DOCKER_REPO" bash hooks/build
+    popd
+
+    Cleanup
+done
